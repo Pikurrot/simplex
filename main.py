19e24ff78fef282532d637290a04e735fe3c85ac
Eric Lopez <pikurrot@gmail.com>
2023-09-23 22:51:02 +0200

    Build tableau

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import numpy as np
 import re
 import utils
+from typing import Iterable
 
 class Simplex:
-	def __init__(self, objective: str, constraints: iter):
+	def __init__(self, objective: str, constraints: Iterable):
+		# Parse objective and constraints
 		self.objective = utils.parse_polynomial(objective)
 		self.constraints = [utils.parse_linear_equation(constraint) for constraint in constraints]
 
+		# Convert to equalities
+		self.objective = utils.convert_to_equality(*self.objective, -1, 't')
+		self.basic_vars = [f's{i}' for i in range(len(self.constraints))]
+		self.constraints = [utils.convert_to_equality(*constraint, self.basic_vars[i]) for i, constraint in enumerate(self.constraints)]
+
+		# Build tableau
+		self.tableau, self.vars = self.build_tableau()
+
+	def build_tableau(self):
+		'''Build the tableau for the current equations. Returns the matrix and the variables corresponding to the columns'''
+		# Extract unique variables
+		all_vars = set(self.objective[0])
+		for eq in self.constraints:
+			all_vars.update(eq[0])
+		all_vars = sorted(list(all_vars))
+		
+		# Build the matrix
+		matrix = np.array([
+			[eq[1][eq[0].index(var)] if var in eq[0] else 0. for var in all_vars] + [eq[2]]
+			for eq in [self.objective] + self.constraints
+		], dtype=np.float64)
+
+		return matrix, all_vars + ['const']
+	
+	def solution(self):
+		'''Get the solutions for the current tableau as a dict'''
+		return dict(zip(['t'] + self.basic_vars, self.tableau[:, -1]))
+
 def main():
-	objective = 'x + y' # for not just maximize
+	objective = 'x + y' # for now just maximize
 	constraints = (
 		'x <= 4',
 		'x + 2y <= 10',
@@ -16,6 +46,7 @@ def main():
 	)
 
 	simplex = Simplex(objective, constraints)
+	print(simplex.solution())
 
 if __name__ == '__main__':
 	main()
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def parse_linear_equation(equation: str):
 	'''Parse a linear equation and return the variables, coefficients, constant, and operator of a linear equation'''
 	# Adjusted regex patterns
 	term_pattern = r'([\+-]?\s*\d*\.?\d*)\s*([a-zA-Z])'
-	operator_pattern = r'([<=>]+)'
+	operator_pattern = r'([<=]+)'
 	constant_pattern = r'([\+-]?\s*\d+\.?\d*)$'  # The constant is expected to be at the end
 
 	# Extract terms, operator, and constant
@@ -14,7 +14,7 @@ def parse_linear_equation(equation: str):
 	constant = float(re.search(constant_pattern, equation).group(1).strip())
 
 	# Convert operator to integer representation
-	operator_map = {'<=': -1, '=': 0, '>=': 1}
+	operator_map = {'=': 0, '<=': 1}
 	operator = operator_map.get(operator_str, None)
 	if operator is None:
 		raise ValueError(f"Unsupported operator: {operator_str}")
@@ -30,7 +30,7 @@ def parse_linear_equation(equation: str):
 		variables.append(variable)
 		coefficients.append(coefficient)
 	
-	return np.array(variables), np.array(coefficients), constant, operator
+	return variables, np.array(coefficients, dtype=np.float64), constant, operator
 
 
 def parse_polynomial(expression: str):
@@ -55,4 +55,16 @@ def parse_polynomial(expression: str):
 		variables.append(variable)
 		coefficients.append(coefficient)
 	
-	return np.array(variables), np.array(coefficients), constant
+	return variables, np.array(coefficients, dtype=np.float64), constant
+
+
+def convert_to_equality(variables: list, coefficients: np.ndarray, constant: float, operator: int, slack_var: str):
+	'''Convert a polynomial or linear inequality to an equality'''
+	if operator == 0:
+		return variables, coefficients, constant
+
+	# Add slack variable
+	variables.append(slack_var)
+	coefficients = np.append(coefficients * operator, 1.0) # if polynomial, negate coefficients
+
+	return variables, coefficients, constant
